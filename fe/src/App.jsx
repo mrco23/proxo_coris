d7c0b4b903d1f15107c30c97adf9035bc811dc14
marcoismakalew <marcoismakalew@gmail.com>
2026-02-20 14:32:59 +0800

    set up landing page

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
--- fe/src/pages/LandingPage.jsx
+++ fe/src/pages/LandingPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function LandingPage() {
-	return <div></div>;
+	return (
+		<div className="min-w-screen min-h-screen flex items-center justify-center">
+			<h1 className="text-3xl font-semibold cursor-pointer hover:scale-110 transition-all">
+				Hello Marco :)
+			</h1>
+		</div>
+	);
 }
 
 export default LandingPage;
