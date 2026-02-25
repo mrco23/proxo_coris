db70650d3c30e177f94b2a2f92abf015e64060b1
Algy <algy25ng@gmail.com>
2026-02-25 23:25:25 +0800

    edit(header): Implement dynamic styling based on scroll position and current page.

--- fe/src/components/layouts/partials/header/Header.jsx
+++ fe/src/components/layouts/partials/header/Header.jsx
@@ -3,13 +3,22 @@ import HeaderLogo from "./HeaderLogo";
 import HeaderNav from "./HeaderNav";
 import HeaderAuth from "./HeaderAuth";
 import MobileMenu from "./MobileMenu";
+import { useLocation } from "react-router-dom";
 
 function Header({ isAuthenticated, user, onLogout }) {
   const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
+  const isLandingPage = location.pathname === "/";
+
+  const [scrolled, setScrolled] = useState(false);
+  const handleScroll = () => {
+    setScrolled(window.scrollY > 50);
+  };
+  window.addEventListener("scroll", handleScroll);
 
   return (
     <nav
-      className={`fixed top-0 right-0 left-0 z-50 flex h-24 w-full justify-center px-4 py-2 md:px-6 ${menuOpen ? "bg-white" : "bg-transparent backdrop-blur-sm"}`}
+      className={`fixed top-0 right-0 left-0 z-50 flex h-24 w-full justify-center px-4 py-2 md:px-6 ${menuOpen || !isLandingPage ? "bg-white shadow-md" : scrolled ? "bg-transparent shadow-md backdrop-blur-sm" : "bg-transparent backdrop-blur-sm"}`}
     >
       <div className="mx-auto flex w-full max-w-6xl items-center justify-between">
         <HeaderLogo />
